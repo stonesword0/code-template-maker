642982ebcd7533363bf03fea611b626612aa3f08
RealStoneSword <58371315+RealStoneSword@users.noreply.github.com>
2020-08-19 03:39:21 -0500

    v1.0
    HTML and Javascript added.

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 
 
 int main() {
+
     /* Making the variable for the 
     type of language the user wants*/
+
     char lang[255];
     printf("\nWhat programming language would you like a template for?\nCurrent suppourted languages: C, Java, Javascript, HTML\n");
     scanf("%s", lang);
@@ -23,7 +25,7 @@ if( strcmp(lang, "java") == 0)
 
     fclose(fpointer);
 
-    printf("Template making successful.\nThanks for using my tool!\n");
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
 }
 
 /* This else statment has a uppercase
@@ -37,7 +39,7 @@ else if( strcmp(lang, "Java") == 0){
 
     fclose(fpointer);
 
-    printf("Template making successful.\nThanks for using my tool!\n");
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
 }
 
 else if( strcmp(lang, "c") == 0){
@@ -48,7 +50,7 @@ else if( strcmp(lang, "c") == 0){
 
     fclose(fpointer);
 
-    printf("Template making successful.\nThanks for using my tool!\n");
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
 }
 
 /* This else statment has a uppercase
@@ -62,7 +64,74 @@ else if( strcmp(lang, "C") == 0){
 
     fclose(fpointer);
 
-    printf("Template making successful.\nThanks for using my tool!\n");
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
+}
+
+else if( strcmp(lang, "javascript") == 0){
+
+    FILE * fpointer = fopen("javascript-template.js", "w");
+
+    fprintf(fpointer, "<!DOCTYPE HTML>\n<html>\n\n<body>\n\n   <script>\n   \n   </script>\n\n</body>\n\n</html>");
+
+    fclose(fpointer);
+
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
+}
+/* This else statment has a uppercase
+J and S just in case the user puts them. */
+
+else if( strcmp(lang, "JavaScript") == 0){
+
+    FILE * fpointer = fopen("javascript-template.js", "w");
+
+    fprintf(fpointer, "<!DOCTYPE HTML>\n<html>\n\n<body>\n\n   <script>\n   \n   </script>\n\n</body>\n\n</html>");
+
+    fclose(fpointer);
+
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
+}
+/* This else statment has a uppercase
+J just in case the user puts one. */
+
+else if( strcmp(lang, "Javascript") == 0){
+
+    FILE * fpointer = fopen("javascript-template.js", "w");
+
+    fprintf(fpointer, "<!DOCTYPE HTML>\n<html>\n\n<body>\n\n   <script>\n   \n   </script>\n\n</body>\n\n</html>");
+
+    fclose(fpointer);
+
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
+}
+
+else if( strcmp(lang, "html") == 0){
+
+    FILE * fpointer = fopen("javascript-template.html", "w");
+
+    fprintf(fpointer, "<!DOCTYPE HTML>\n<html>\n\n   <body>\n   \n   \n   </body>\n\n</html>");
+
+    fclose(fpointer);
+
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
+}
+
+/* This else statment has a uppercase
+HTML just in case the user puts one. */
+
+else if( strcmp(lang, "HTML") == 0){
+
+    FILE * fpointer = fopen("javascript-template.html", "w");
+
+    fprintf(fpointer, "<!DOCTYPE HTML>\n<html>\n\n   <body>\n   \n   \n   </body>\n\n</html>");
+
+    fclose(fpointer);
+
+    printf("Template making successful.\nThanks for using my tool!\nYour template is in the folder you ran the program from.\n");
+}
+
+else{
+    printf("Incorrect language. Please try again.\n");
+
 }
 
     return 0;
